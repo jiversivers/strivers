5fc89a14928918f67319bf51c5fa24e40a522756
jiversivers <iversjesse@gmail.com>
2025-01-15 16:59:44 -0600

    updated name refs following rename

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for MySite project.
+URL configuration for master-site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
