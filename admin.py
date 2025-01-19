5e7a0561d086df53f4cf060149945a1989207806
jiversivers <iversjesse@gmail.com>
2025-01-19 11:55:20 -0600

    added strivers models to admin

--- admin.py
+++ admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from strivers.models import Athlete, ActivityOverview, ActivityData
+
 # Register your models here.
+admin.site.register(Athlete)
+admin.site.register(ActivityOverview)
+admin.site.register(ActivityData)
