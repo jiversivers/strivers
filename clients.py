d2a5a6214888ac3fb40800acefdad4b9f8ba4c2b
jiversivers <iversjesse@gmail.com>
2025-01-19 11:56:14 -0600

    renamed client to configured_client for clarity at usage

--- clients.py
+++ clients.py
@@ -2,4 +2,4 @@ import swagger_client as sc
 
 # Instantiate global swagger client for api calls
 configuration = sc.Configuration()
-client = sc.ApiClient(configuration)
+configured_client = sc.ApiClient(configuration)
