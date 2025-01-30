f51a722a618b3ebd6ec703c28d8784e6aa63258e
jiversivers <iversjesse@gmail.com>
2025-01-30 15:46:28 -0600

    updated session variables for ease of use and middleware compatibility.

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ import json
 from decouple import config
 
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 import swagger_client as sc
@@ -15,12 +15,12 @@ from strivers.models import Athlete, ActivityOverview
 # Check for authentication state and route accordingly
 def index(request):
     # Session authed...ready to go to home
-    if 'access_token' in request.session:
+    if 'athlete' in request.session:
         return redirect('strivers:home')
 
     # User authed, session not...need to look up and set session. Then go to home
     elif 'user_id' in request.COOKIES:
-        request.session['access_token'] = Athlete.objects.get(pk=request.COOKIES['user_id'])
+        request.session['athlete'] = Athlete.objects.get(pk=request.COOKIES['user_id'])
         return redirect('strivers:home')
 
     # Not authed at all...send to authorization view
@@ -31,22 +31,24 @@ def index(request):
 def home(request):
     context = {}
     # If session is not authorized
-    if 'access_token' not in request.session and 'user_id' not in request.COOKIES:
+    if 'athlete' not in request.session and 'user_id' not in request.COOKIES:
             context['option_action'] = [('Authorize on Strava', reverse('strivers:authorize'))]
 
     # If there is a cookie, set session to match and load athlete details
     if 'user_id' in request.COOKIES:
-        request.session['access_token'] = Athlete.objects.get(pk=request.COOKIES['user_id']).access_token
+        athlete = Athlete.objects.get(pk=request.COOKIES['user_id'])
+        request.session['athlete'] = athlete
         context['issaved'] = True
-        context['username'] = Athlete.objects.get(athlete_id=request.session['athlete']['id']).username
+        context['name'] = athlete.first_name
 
     # If the session is authorized
-    if 'access_token' in request.session:
+    if 'athlete' in request.session:
+        athlete = request.session['athlete']
         if 'username' not in context:
-            context['username'] = request.session['athlete']['username']
+            context['name'] = athlete.first_name
 
         # At least some activities have been loaded for the athlete
-        if ActivityOverview.objects.filter(athlete_id=request.session['athlete']['id']).count() > 0:
+        if ActivityOverview.objects.filter(athlete_id=athlete.id).count() > 0:
             context['option_action'] = [('Update Activities', reverse('strivers:get_activities')),
                                         ('Analyze', reverse('strivers:analysis_tools'))]
 
@@ -61,7 +63,7 @@ def home(request):
 
 def authorize(request):
     client_id = config('CLIENT_ID')
-    redirect_uri = 'http://localhost:8000/strivers/callback/'
+    redirect_uri = config('DOMAIN') + '/strivers/callback/'
     scope = 'activity:read_all'
     response_type = 'code'
 
@@ -70,7 +72,6 @@ def authorize(request):
         f"?client_id={client_id}&redirect_uri={redirect_uri}"
         f"&response_type={response_type}&scope={scope}"
     )
-
     return HttpResponseRedirect(strava_url)
 
 def authorization_callback(request):
@@ -85,6 +86,10 @@ def authorization_callback(request):
     # Check if full scope was given and confirm selection if not
     if scope != 'read,activity:read_all' and ('confirm_limit' not in request.POST or request.POST.get('confirm_limit') == 'reauthorize'):
         return render(request, 'strivers/confirm_limit.html')
+    elif scope != 'read,activity:read_all':
+        scope = False
+    else:
+        scope = True
 
     # Exchange code for user access token
     token_url = 'https://www.strava.com/oauth/token'
@@ -97,12 +102,16 @@ def authorization_callback(request):
     response = requests.post(token_url, data=data)
     response_data = response.json()
 
-    # Store athlete access info into session (note: swagger client configuration will be updated
-    request.session['access_token'] = response_data.get('access_token')
-    request.session['refresh_token'] = response_data.get('refresh_token')
-    request.session['expires_in'] = response_data.get('expires_in')
-    request.session['athlete'] = response_data.get('athlete')
-
+    # Store athlete info into session (note: swagger client configuration will be updated in middleware)
+    # (model of data only saved in db past session if cookie is accepted)
+    request.session['athlete'] = {'athlete_id': response_data['athlete']['id'],
+                                  'username': response_data['athlete']['username'],
+                                  'first_name': response_data['athlete']['firstname'],
+                                  'last_name': response_data['athlete']['lastname'],
+                                  'access_token': response_data['access_token'],
+                                  'refresh_token': response_data['refresh_token'],
+                                  'expires_at': response_data['expires_at'],
+                                  'scope': scope}
 
     # Ask about a cookie
     return render(request,'strivers/cookie_quest.html')
@@ -110,30 +119,32 @@ def authorization_callback(request):
 def store_cookie(request):
     # Default behavior no matter what
     response = redirect('strivers:home')
-    # Set the cookie
+    athlete = request.session['athlete']
+
+    # Set the cookie and save to/update database
     if request.method == 'POST' and request.POST['cookie'] == 'yes':
-        expires_at = datetime.now() + timedelta(seconds=request.session['expires_in'])
 
         # Check if athlete is already in database (even if reauthorizing)
-        if Athlete.objects.filter(athlete_id=request.session['athlete']['id']).count() == 0:
-            # Put profile and access info into a db indexed by athlete ID
-            user_info = Athlete(athlete_id=request.session['athlete']['id'],
-                                username=request.session['athlete']['username'],
-                                first_name=request.session['athlete']['firstname'],
-                                last_name=request.session['athlete']['lastname'],
-                                access_token=request.session.get('access_token'),
-                                refresh_token=request.session['refresh_token'],
-                                expires_at=expires_at)
-            user_info.save()
+        if Athlete.objects.filter(athlete_id=request.session['athlete']['athlete_id']).count() == 0:
+            athlete_model = Athlete(athlete_id=str(request.session['athlete']['athlete_id']),
+                                  username=request.session['athlete']['username'],
+                                  first_name=request.session['athlete']['first_name'],
+                                  last_name=request.session['athlete']['last_name'],
+                                  access_token=request.session['athlete']['access_token'],
+                                  refresh_token=request.session['athlete']['refresh_token'],
+                                  expires_at=datetime.fromtimestamp(request.session['athlete']['expires_at']),
+                                  scope=request.session['athlete']['scope'])
+            athlete_model.save()
         else:
-            user_info = Athlete.objects.get(athlete_id=request.session['athlete']['id'])
+            athlete_model = Athlete.objects.get(athlete_id=request.session['athlete']['athlete_id'])
 
         # Place the cookie (unique pk in Athlete objects db)
         response.set_cookie('user_id',
-                            str(user_info.id),
+                            str(athlete_model.id),
                             max_age=60 * 60 * 24 * 30, # 30 days
                             httponly=True,
                             secure=True)
+
     return response
 
 # TODO: Set up database to store where activity loading left off to accommodate updating activities list
