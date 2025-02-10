f0c03d38e920bfb77e473636d567793ae2058a1c
jiversivers <iversjesse@gmail.com>
2025-02-10 07:19:12 -0600

    placeholder tests started

--- tests.py
+++ tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
 # Create your tests here.
+def IndexViewTest(TestCase):
+    def test_first_time(self):
+        # No cookie or session ID
+
+        # Should return auth page
+        pass
+    def test_cookie(self):
+        # Only cookie
+
+        # Should return home
+        pass
+
+    def test_session(self):
+        # Only session
+
+        # Should return home
+        pass
+
+def HomeViewTest(TestCase):
+    def test_first_time(self):
+        # No cookie or session ID
+
+        # Should offer auth page
+        pass
+
+    def test_cookie(self):
+        # Only cookie
+
+        # Should offer actions and logout
+        pass
+
+    def test_session(self):
+        # Only session
+
+        # Should offer actions
+        pass
